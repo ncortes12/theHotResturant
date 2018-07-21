3107aa82fd1608d895a990127626c1982d527534
Nathan Novak <nnovak@ku.edu>
2018-07-21 13:25:34 -0700

    updated

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ app.get("/reservation", function(req, res) {
 
 app.get("/", function(req, res) {
 	res.sendFile(path.join(__dirname, "index.html"));
+	
 });
 
 app.get("/tables", function(req, res) {
